33dbb97911824911e2da94a63f67e0729f907408
Christophe Meessen <christophe@meessen.net>
2019-12-01 13:00:51 +0100

    set correct sols size

--- main.go
+++ main.go
@@ -7,16 +7,10 @@ import (
 	"time"
 )
 
-const maxN = 13
+const maxLineLength = 13
 
 // CellColor is a cell color.
 type CellColor byte
-
-// CellLine is a line of CellColor.
-type CellLine []CellColor
-
-// CellMatrix is a matrix of CellColor.
-type CellMatrix []CellLine
 
 const (
 	greyCell CellColor = iota
@@ -24,83 +18,96 @@ const (
 	whiteCell
 )
 
-// Kakurasu is a kakurasu.
-type Kakurasu struct {
-	n       int
-	rowSums []int
-	colSums []int
-	m       CellMatrix
+// CellLine is a line of CellColor.
+type CellLine []CellColor
+
+// CellGrid is an n rows by m columns grid of CollColor.
+type CellGrid struct {
+	n, m int
+	g    []CellLine
 }
 
-// return a CellMatrix with grey cells
-func newCellMatrix(n int) CellMatrix {
-	m := make([]CellLine, n)
-	for i := range m {
-		m[i] = make([]CellColor, n)
+// NewCellGrid returns a CellMatrix with grey cells.
+func NewCellGrid(n, m int) *CellGrid {
+	g := make([]CellLine, n)
+	for i := range g {
+		g[i] = make(CellLine, m)
 	}
-	return m
+	return &CellGrid{n: n, m: m, g: g}
 }
 
-// NewKakurasu return a new random Kakurasu.
-func NewKakurasu(n int) *Kakurasu {
-	if n > maxN {
-		panic(fmt.Sprint("n too big:", n))
+// Clone return a copy of the CellGrid.
+func (g *CellGrid) Clone() *CellGrid {
+	newG := &CellGrid{n: g.n, m: g.m, g: make([]CellLine, g.n)}
+	for i := range newG.g {
+		newG.g[i] = append(newG.g[i], g.g[i]...)
 	}
-	k := &Kakurasu{n: n, rowSums: make([]int, n), colSums: make([]int, n)}
-	k.m = newCellMatrix(n)
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
+	return newG
+}
+
+// FillRandomly fills grid randomly with black and white cell colors.
+func (g *CellGrid) FillRandomly() {
+	for i := 0; i < g.n; i++ {
+		for j := 0; j < g.m; j++ {
 			if rand.Intn(2) == 1 {
-				k.rowSums[i] += j+1 // lines
-				k.colSums[j] += i+1 // culumns
-				k.m[i][j] = blackCell
+				g.g[i][j] = blackCell
 			} else {
-				k.m[i][j] = whiteCell
+				g.g[i][j] = whiteCell
 			}
 		}
 	}
-	return k
 }
 
-// Print outputs the Kakurasu to stdOut.
-func (k *Kakurasu) Print() {
-	fmt.Print("  ")
-	for j := 0; j < k.n; j++ {
-		fmt.Printf("%2d", k.colSums[j])
+// ComputeSums of black cell weight in rows and columns.
+func (g *CellGrid) ComputeSums() (rowSums, colSums []int) {
+	rowSums = make([]int, g.n)
+	colSums = make([]int, g.m)
+	for i := 0; i < g.n; i++ {
+		for j := 0; j < g.m; j++ {
+			if g.g[i][j] == blackCell {
+				rowSums[i] += j + 1
+				colSums[j] += i + 1
+			}
+		}
 	}
-	for i := 0; i < k.n; i++ {
-		fmt.Print("\n  ")
-		for j := 0; j < k.n; j++ {
-			switch k.m[i][j] {
+	return
+}
+
+// Print outputs CellGrid to stdOut.
+func (g *CellGrid) Print() {
+	for i := 0; i < g.n; i++ {
+		for j := 0; j < g.m; j++ {
+			switch g.g[i][j] {
 			case blackCell:
-				fmt.Print("**")
+				fmt.Print("*")
 			case whiteCell:
-				fmt.Print("  ")
+				fmt.Print(" ")
 			case greyCell:
-				fmt.Print("??")
+				fmt.Print("?")
 			default:
 				panic("invalid cell color")
 			}
 		}
-		fmt.Printf("\n%2d", k.rowSums[i])
-		for j := 0; j < k.n; j++ {
-			switch k.m[i][j] {
-			case blackCell:
-				fmt.Print("**")
-			case whiteCell:
-				fmt.Print("  ")
-			case greyCell:
-				fmt.Print("??")
-			default:
-				panic("invalid cell color")
-			}
-		}
+		fmt.Println()
+	}
+}
+
+//PrintSums prints the sums to stdOut.
+func PrintSums(rowSums, colSums []int) {
+	fmt.Print("row sums:")
+	for i := range rowSums {
+		fmt.Printf(" %2d", rowSums[i])
+	}
+	fmt.Println()
+	fmt.Print("col sums:")
+	for i := range colSums {
+		fmt.Printf(" %2d", colSums[i])
 	}
 	fmt.Println()
 }
 
 func buildSolutions(n int) map[int][]CellLine {
-	if n > maxN {
+	if n > maxLineLength {
 		panic(fmt.Sprint("n too big:", n))
 	}
 	m := make(map[int][]CellLine)
@@ -151,7 +158,7 @@ func printSolutions(solTbl map[int][]CellLine) {
 	}
 }
 
-func solveLine(sols []CellLine) CellLine {
+func deduceColorsFormSols(sols []CellLine) CellLine {
 	nSols := len(sols)
 	if nSols == 0 {
 		panic("line has no solutions")
@@ -160,29 +167,29 @@ func solveLine(sols []CellLine) CellLine {
 		return sols[0]
 	}
 	n := len(sols[0])
-    c := make(CellLine, n)
-    for i := range c {
-    	var acc int
-    	for j := range sols {
-    		if sols[j][i] == blackCell{
-    			acc++
-    		}
-    	}
-    	switch acc {
-    	case 0: 
-    	    c[i] = whiteCell
-    	case nSols:
-    	    c[i] = blackCell
-    	default:
-    	    c[i] = greyCell
-    	}
-    }
+	c := make(CellLine, n)
+	for i := range c {
+		var acc int
+		for j := range sols {
+			if sols[j][i] == blackCell {
+				acc++
+			}
+		}
+		switch acc {
+		case 0:
+			c[i] = whiteCell
+		case nSols:
+			c[i] = blackCell
+		default:
+			c[i] = greyCell
+		}
+	}
 	return c
 }
 
-// eliminate solutions incompatible with color c at index k
-func filterLineSols(sols []CellLine, k int, c CellColor) []CellLine {
-    var j int
+// eliminate solutions incompatible with black or white color c at index k.
+func filterSols(sols []CellLine, k int, c CellColor) []CellLine {
+	var j int
 	for i := range sols {
 		if sols[i][k] == c {
 			sols[j] = sols[i]
@@ -192,97 +199,148 @@ func filterLineSols(sols []CellLine, k int, c CellColor) []CellLine {
 	return sols[:j]
 }
 
-func lineStr(l CellLine) string {
-	c := make([]byte, len(l))
-	for i := range l {
-		switch l[i]{
-		case blackCell:
-		    c[i] = '*'
-		case whiteCell:
-		    c[i] = ' '
-		default:
-		    c[i] = '?'
-		}
+func initSols(sums []int, m int) [][]CellLine {
+	n := len(sums)
+	solTbl := buildSolutions(m)
+	sols := make([][]CellLine, n)
+	for i := 0; i < n; i++ {
+		sols[i] = append(sols[i], solTbl[sums[i]]...)
 	}
-	return string(c)
+	return sols
 }
 
-
-// Solve Kakurasu and return solution matrix.
-func Solve(rowSums, colSums []int) CellMatrix {
-	n := len(rowSums)
-	if len(colSums) != n {
-		panic("solve only square Kakurasu")
+func copySols(sols [][]CellLine) [][]CellLine {
+	newSols := make([][]CellLine, len(sols))
+	for i := range newSols {
+		newSols[i] = append(newSols[i], sols[i]...)
 	}
+	return newSols
+}
 
-	solTbl := buildSolutions(n)
-	// printSolutions(solTbl)
+type solveState struct {
+	rowSols [][]CellLine
+	colSols [][]CellLine
+	g       *CellGrid
+	nGreys  int
+}
 
-	m := newCellMatrix(n)
-
-	// copy solutions into rowSols and colSols
-	rowSols := make([][]CellLine, n)
-	colSols := make([][]CellLine, n)
-	for i := 0; i < n; i++ {
-		rowSols[i] = append(rowSols[i], solTbl[rowSums[i]]...)
-		colSols[i] = append(colSols[i], solTbl[colSums[i]]...)
+func newSolveState(rowSums, colSums []int) *solveState {
+	return &solveState{
+		rowSols: initSols(rowSums, len(colSums)),
+		colSols: initSols(colSums, len(rowSums)),
+		g:       NewCellGrid(len(rowSums), len(colSums)),
+		nGreys:  len(rowSums) * len(colSums),
 	}
+}
 
+func (s *solveState) clone() *solveState {
+	return &solveState{
+		rowSols: copySols(s.rowSols),
+		colSols: copySols(s.colSols),
+		g:       s.g.Clone(),
+		nGreys:  s.nGreys,
+	}
+}
+
+func (s *solveState) findFirstGrey() (r, c int) {
+	for r := 0; r < s.g.n; r++ {
+		for c := 0; c < s.g.m; c++ {
+			if s.g.g[r][c] == greyCell {
+				return r, c
+			}
+		}
+	}
+	panic("no grey cell found")
+}
+
+func (s *solveState) setFirstGreyColorTo(clr CellColor) {
+	r, c := s.findFirstGrey()
+	s.g.g[r][c] = clr
+	s.nGreys--
+	s.rowSols[r] = filterSols(s.rowSols[r], c, clr)
+	s.colSols[c] = filterSols(s.colSols[c], r, clr)
+}
+
+func (s *solveState) deduce() {
 	hasMod := true
 	// as long as we have modified rows or columns
 	for hasMod {
 		hasMod = false
-		// for rows
-		for r := 0; r < n; r++ {
-			sol := solveLine(rowSols[r])
-			for c := 0; c < n; c++ {
+		// for each row
+		for r := 0; r < s.g.n; r++ {
+			sol := deduceColorsFormSols(s.rowSols[r])
+			for c := 0; c < s.g.m; c++ {
 				if sol[c] == greyCell {
 					continue
 				}
-				if m[r][c] == greyCell {
+				if s.g.g[r][c] == greyCell {
 					// we solved a new cell
 					hasMod = true
-					m[r][c] = sol[c]
-					colSols[c] = filterLineSols(colSols[c], r, sol[c])
+					s.nGreys--
+					s.g.g[r][c] = sol[c]
+					s.colSols[c] = filterSols(s.colSols[c], r, sol[c])
 				} else {
-					if m[r][c] != sol[c] {
+					if s.g.g[r][c] != sol[c] {
 						panic("matrix and solution mismatch")
 					}
 				}
 			}
 		}
-		// for columns
-		for c := 0; c < n; c++ {
-			sol := solveLine(colSols[c])
-			for r := 0; r < n; r++ {
+		// for each column
+		for c := 0; c < s.g.m; c++ {
+			sol := deduceColorsFormSols(s.colSols[c])
+			for r := 0; r < s.g.n; r++ {
 				if sol[r] == greyCell {
 					continue
 				}
-				if m[r][c] == greyCell {
+				if s.g.g[r][c] == greyCell {
 					// we solved a new cell
 					hasMod = true
-					m[r][c] = sol[r]
-					rowSols[r] = filterLineSols(rowSols[r], c, sol[r])
+					s.nGreys--
+					s.g.g[r][c] = sol[r]
+					s.rowSols[r] = filterSols(s.rowSols[r], c, sol[r])
 				} else {
-					if m[r][c] != sol[r] {
+					if s.g.g[r][c] != sol[r] {
 						panic("matrix and solution mismatch")
 					}
 				}
 			}
 		}
 	}
-	return m
+}
+
+func (s *solveState) doSolve(sols []*CellGrid) []*CellGrid {
+	s.deduce()
+	if s.nGreys == 0 {
+		return append(sols, s.g)
+	}
+	s2 := s.clone()
+	s.setFirstGreyColorTo(blackCell)
+	s2.setFirstGreyColorTo(whiteCell)
+	return s2.doSolve(s.doSolve(sols))
+}
+
+// Solve Kakurasu and return list of solution grids.
+func Solve(rowSums, colSums []int) []*CellGrid {
+	s := newSolveState(rowSums, colSums)
+	return s.doSolve([]*CellGrid{})
 }
 
 func main() {
-	n := 5
 	rand.Seed(time.Now().Unix())
-	k := NewKakurasu(n)
-	k.Print()
+	g := NewCellGrid(5, 6)
+	g.FillRandomly()
+	rowSums, colSums := g.ComputeSums()
 
-	fmt.Println("solution:")
-	k2 := k
-	k2.m = Solve(k.rowSums, k.colSums)
-	k2.Print()
+	fmt.Println("Kakurasu:")
+	g.Print()
+	PrintSums(rowSums, colSums)
+	fmt.Println()
 
+	sols := Solve(rowSums, colSums)
+	fmt.Println("Solutions:")
+	for i := range sols {
+		sols[i].Print()
+		fmt.Println()
+	}
 }
